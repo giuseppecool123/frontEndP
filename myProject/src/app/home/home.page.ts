28da80e3f1f150f2b5ee092e0c6d679e5acf1299
giuseppecool123 <g00421024@atu.ie>
2025-04-22 20:02:36 +0100

    Using the Lab 6 Navigation code i got router link to work making the buttons fully functional in my home.html , imported routerlink and module into home.page.ts also

--- myProject/src/app/home/home.page.ts
+++ myProject/src/app/home/home.page.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton , IonIcon } from '@ionic/angular/standalone';
+import { RouterLink, RouterModule } from '@angular/router'; // ✅ important
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton , IonIcon, RouterModule, RouterLink],
 })
 export class HomePage {
-  constructor() {}
+  constructor() {} 
 }
