a67fff8278553cdcae056ad682f3ae7f67cc9013
giuseppecool123 <g00421024@atu.ie>
2025-04-22 17:16:24 +0100

    new pages

--- myProject/src/app/app.routes.ts
+++ myProject/src/app/app.routes.ts
@@ -10,4 +10,12 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'settings',
+    loadComponent: () => import('./settings/settings.page').then( m => m.SettingsPage)
+  },
+  {
+    path: 'game',
+    loadComponent: () => import('./game/game.page').then( m => m.GamePage)
+  },
 ];
